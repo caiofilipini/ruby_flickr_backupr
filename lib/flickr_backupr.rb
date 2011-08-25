5c8270e0850ee8b0caf59165f1a21cd006b54df9
Caio Filipini <caiofilipini@gmail.com>
2011-08-25 19:18:41 -0300

    fixing wrong paths in require

--- lib/flickr_backupr.rb
+++ lib/flickr_backupr.rb
@@ -3,8 +3,8 @@ require "yaml"
 
 CONFIG = YAML.load_file("config.yml")
 
-require File.expand_path("flickr_backupr/photo_downloadr")
-require File.expand_path("flickr_backupr/local_image")
-require File.expand_path("flickr_backupr/backupr")
+require File.expand_path("lib/flickr_backupr/photo_downloadr")
+require File.expand_path("lib/flickr_backupr/local_image")
+require File.expand_path("lib/flickr_backupr/backupr")
 
 Backupr.backup!
